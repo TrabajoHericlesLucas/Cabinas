d7d42e2504596894e14dda267a78fb1cce5a5d35
admin <admin@Win7_1TB>
2021-04-19 14:31:39 +0300

    Primer commit

---
+++ trabajoEntornos/src/trabajoEntornos/Main.java
@@ -0,0 +1,10 @@
+package trabajoEntornos;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("probando Commit");
+	}
+
+}
