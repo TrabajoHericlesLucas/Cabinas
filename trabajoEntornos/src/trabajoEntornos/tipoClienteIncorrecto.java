927a117cd988bc85d4f89950cc682cc22c1e8e73
W7 <W7@W7-PC>
2021-05-01 19:49:46 +0200

    Trabajo Lucas Sabado

---
+++ trabajoEntornos/src/trabajoEntornos/tipoClienteIncorrecto.java
@@ -0,0 +1,13 @@
+package trabajoEntornos;
+
+public class tipoClienteIncorrecto extends Exception {
+	
+	public tipoClienteIncorrecto() {
+		super();
+	}
+	
+	public tipoClienteIncorrecto(String message) {
+		super(message);
+	}
+		
+}
