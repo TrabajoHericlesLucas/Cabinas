a08afbe44d7b4de6c3cb92d9b4e221ca16e80e4a
Héricles <Hricles@DESKTOP-1EGONL5>
2021-05-02 17:26:32 +0200

    cambios hericles

--- trabajoEntornos/src/trabajoEntornos/Cliente.java
+++ trabajoEntornos/src/trabajoEntornos/Cliente.java
@@ -4,4 +4,14 @@ import java.util.Vector;
 
 public class Cliente extends Vector {
 
+	private String matricula;
+	
+	private char tipo;
+	
+	public Cliente(String matricula, char tipo) {
+		super();
+		this.matricula = matricula;
+		this.tipo = tipo;
+	}
+	
 }
